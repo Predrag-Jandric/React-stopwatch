831505f7b9805585c3c183409fd61d8af3ce5e5e
predrag jandric <predrag.jandric.bg@gmail.com>
2024-07-16 23:29:31 +0200

    ADDED: full stopwatch logic

---
+++ README.md
@@ -0,0 +1,26 @@
+# React - Stopwatch
+
+# 🔗 [Live Preview]()
+
+![Design preview](./public/preview.png)
+
+---
+
+## About 👋
+
+A simple stopwatch application built with React. The application allows users to start, stop, and reset the stopwatch. The user interface is designed with modern styling for a better user experience.
+
+---
+
+## Features 👨‍💻
+
+- Start and stop the stopwatch
+- Reset the stopwatch
+- Responsive and modern design
+
+---
+
+## Languages
+
+- React: functions, conditionals, useState, useEffect
+- Styling: basic
---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  function handleReset() {
+    setTime(0);
+    setRunning(false);
+  }
+
+  useEffect(() => {
+    let interval;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prev) => prev + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <div className="time">
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+
+      <div className="buttons">
+        {running ? (
+          <button onClick={() => setRunning(false)}>Stop </button>
+        ) : (
+          <button onClick={() => setRunning(true)}>Start</button>
+        )}
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
